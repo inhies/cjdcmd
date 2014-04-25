33dca6207dd60fcec6ea33820977bd1cc00d9d5c
Emery Hemingway <emery@vfemail.net>
2014-04-25 14:58:13 -0400

    new dump format

--- dump.go
+++ dump.go
@@ -16,18 +16,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/inhies/go-cjdns/admin"
 	"github.com/spf13/cobra"
 	"os"
 )
 
+var Pretty bool
+
+func init() {
+	DumpCmd.Flags().BoolVarP(&Pretty, "pretty", "p", false, "pretty output")
+}
+
 func dumpCmd(cmd *cobra.Command, args []string) {
-	admin := Connect()
-	table, err := admin.NodeStore_dumpTable()
+	c := Connect()
+	table, err := c.NodeStore_dumpTable()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
+	if Pretty {
+		dumpTablePretty(table)
+	} else {
+		dumpTablePlain(table)
+	}
+}
+
+func dumpTablePlain(table admin.Routes) {
 	table.SortByQuality()
 	k := 1
 	for _, v := range table {
@@ -39,3 +54,61 @@ func dumpCmd(cmd *cobra.Command, args []string) {
 		}
 	}
 }
+
+func dumpTablePretty(table admin.Routes) {
+	table.SortByPath()
+
+	fmt.Printf("%s┐\n", table[0].Path)
+
+	printPrettySubtable(table[1:], "")
+}
+
+func printPrettySubtable(table admin.Routes, spacer string) {
+	var level []admin.Routes
+
+	for i, here := range table {
+		if here == nil {
+			continue
+		}
+		// Hit each entry once
+		table[i] = nil
+
+		// make a subtable with here at the front
+		sublevel := make(admin.Routes, 1)
+		sublevel[0] = here
+		for j, there := range table {
+			if there == nil {
+				continue
+			}
+
+			if there.Path.IsBehind(*here.Path) {
+				sublevel = append(sublevel, there)
+				table[j] = nil
+			}
+		}
+
+		level = append(level, sublevel)
+	}
+
+	for _, sublevel := range level[:len(level)-1] {
+		here := sublevel[0]
+		if len(sublevel) == 1 {
+			fmt.Printf("%s%s└─%s\n", here.Path, spacer, here.IP)
+			return
+		}
+
+		fmt.Printf("%s%s├┬%s\n", here.Path, spacer, here.IP)
+		printPrettySubtable(sublevel[1:], spacer+"│")
+	}
+
+	sublevel := level[len(level)-1]
+	here := sublevel[0]
+	if len(sublevel) == 1 {
+		fmt.Printf("%s%s└─%s\n", here.Path, spacer, here.IP)
+		return
+	}
+	
+	fmt.Printf("%s%s└┬%s\n", here.Path, spacer, here.IP)
+	printPrettySubtable(sublevel[1:], spacer+" ")
+}
+
